6cf23141bbdf4627069c1543a896328180b2c10b
J-Naish <mjyrgd2332@i.softbank.jp>
2023-01-23 20:50:38 +0900

    Mob follows player if they are close(failed)

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[RequireComponent(typeof(Collider))]
+public class CollisionDetector : MonoBehaviour
+{
+
+
+    [SerializeField] private TriggerEvent onTriggerStay = new TriggerEvent();
+
+    private void OnTriggerStay(Collider other)
+    {
+        onTriggerStay.Invoke(other);
+    }
+
+
+    [Serializable]
+    public class TriggerEvent : UnityEvent<Collider>
+    {
+    }
+
+}
---
+++ Assets/Scripts/MobMove.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MobMove : MonoBehaviour
+{
+
+    
+    // public Transform target;
+    private NavMeshAgent roboAgent;
+
+    void Start()
+    {
+        // Agentを取得
+        roboAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // 衝突で追いかける仕様に変更のためコメントアウト
+    //void Update()
+    //{
+    //    roboAgent.SetDestination(target.position);
+    //}
+
+    public void OnDetectObject(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            // 衝突対象を向く
+            transform.LookAt(collider.transform.position);
+            // 衝突対象に向かう
+            roboAgent.destination = collider.transform.position;            
+        }
+    }
+
+}
