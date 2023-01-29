0cdf7f521a12212129ecf62c3ac6be70fe37f1da
J-Naish <mjyrgd2332@i.softbank.jp>
2023-01-29 16:08:44 +0900

    Setting an enemy larger than previous one and attach collider & EnemyController

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : BasePlayer
+{
+
+
+    void Start()
+    {
+        grossLife = 1000.0f;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
