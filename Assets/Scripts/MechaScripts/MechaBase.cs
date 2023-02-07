e3593898f84275b4a787186033df754a72cec6df
J-Naish <mjyrgd2332@i.softbank.jp>
2023-02-07 21:44:44 +0900

    "MechaButton" become transparent when M-key pushed

--- Assets/Scripts/MechaScripts/MechaBase.cs
+++ Assets/Scripts/MechaScripts/MechaBase.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MechaBase : MonoBehaviour
 {
 
     // 効果持続持続時間に関する変数
     protected float duration;
     protected float currentTime = 0f;
-
-
-    // メカの使用を検知するbool値
-    private bool useMecha = false;
 
 }
---
+++ Assets/Scripts/MechaScripts/MechaUse.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MechaUse : MonoBehaviour
+{
+
+    // メカの使用をキーから検知するための変数
+    public bool useMecha = false;
+
+
+    // メカボタンを取得(メカ所持のbool値を変更するため)
+    [SerializeField] private GameObject mechaEmpty;
+
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.M))
+        {
+            useMecha = true;
+
+            // メカを使用したのでメカボタンを半透明に
+            mechaEmpty.GetComponent<MechaButton>().hasMecha = false;
+        }
+
+    }
+}
