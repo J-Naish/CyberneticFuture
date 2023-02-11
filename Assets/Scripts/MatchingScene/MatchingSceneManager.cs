bc5c92c583d452df7bdf9c1271f7d37f42119abd
J-Naish <mjyrgd2332@i.softbank.jp>
2023-02-12 00:01:01 +0900

    Creating LoadingScene and SceneManager of MatchingScene

---
+++ Assets/Scripts/MatchingScene/MatchingSceneManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+// MatchingSceneの遷移に関するクラス
+public class MatchingSceneManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
