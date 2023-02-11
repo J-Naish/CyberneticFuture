9507d6ecf24473a967964a33809cba16075abdeb
J-Naish <mjyrgd2332@i.softbank.jp>
2023-02-11 21:28:20 +0900

    Creating Scene-Manager of Home-Scene

---
+++ Assets/Scripts/HomeScene/HomeSceneManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// HomeSceneの遷移に関するクラス
+public class HomeSceneManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
