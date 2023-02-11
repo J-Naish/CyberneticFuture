0ac3e1917bab921f27dbce1f8eb4428e10854dcd
J-Naish <mjyrgd2332@i.softbank.jp>
2023-02-11 18:11:59 +0900

    Loin-Text fading away and in

--- Assets/Scripts/LoginScene/LoginTextController.cs
+++ Assets/Scripts/LoginScene/LoginTextController.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class LoginTextController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] private TextMeshProUGUI loginText;
+
+
+    // 透明度を変更するために用意した変数
+    float x = 1.0f;
+    float i = 240.0f;
+
+    
     void Update()
     {
-        
+
+        FadeInAndOut();
+
     }
+
+
+
+    // フェード表示するための関数
+    private void FadeInAndOut()
+    {
+        
+
+        var c = loginText.color;
+        loginText.color = new Color(c.r, c.g, c.b, i/240);
+
+        i -= x;
+
+        // alphaが0になればalpha値を増やしていく
+        if(i == 0f)
+        {
+            x = -1.0f;
+        }
+        // alpha値が1になればalpha値を減らしていく
+        else if(i == 240.0f)
+        {
+            x = 1.0f;
+        }
+
+    }
+
+
 }
