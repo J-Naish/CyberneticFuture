6abeacb87cda57b3133f5db00fe15d7017d3cc92
J-Naish <mjyrgd2332@i.softbank.jp>
2023-02-12 21:01:37 +0900

    Showing Matchmaking-Succesful text

--- Assets/Scripts/MatchingScene/MatchingSceneManager.cs
+++ Assets/Scripts/MatchingScene/MatchingSceneManager.cs
@@ -5,17 +5,61 @@ using UnityEngine.SceneManagement;
 
 
 // MatchingSceneの遷移に関するクラス
+// とりあえず5秒経てばLoadingSceneへ
 public class MatchingSceneManager : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    }
+
+    // 秒数を扱うための変数
+    private float currentTime = 0f;
 
     
     void Update()
     {
-        
+
+        // カウント開始
+        currentTime += Time.deltaTime;
+
+
+        LoadGameScene();
+
+        CancelMatchingToHome();
+
     }
+
+
+
+    // マッチングをキャンセルしてHomeSceneに戻る関数
+    private void CancelMatchingToHome()
+    {
+
+        // 5秒経つまではキャンセルできる
+        if (currentTime < 5.0f)
+        {
+            // Cキーでマッチングキャンセル
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                // キャンセルしたらHomeへ
+                SceneManager.LoadScene("Home");
+            }
+
+        }
+
+    }
+
+
+    // LoadingSceneへ遷移する関数
+    private void LoadGameScene()
+    {
+
+        // 5秒経てばロード開始
+        if(currentTime >= 10.0f)
+        {
+            
+            SceneManager.LoadScene("Loading");
+
+        }
+
+    }
+
+
 }
---
+++ Assets/Scripts/MatchingScene/MatchingScreenController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// マッチング完了画面に関するクラス
+public class MatchingScreenController : MonoBehaviour
+{
+
+    // マッチング画面に用いる画像を取得
+    [SerializeField] private Image[] matchmakingImageArray = new Image[3];
+
+
+    // とりあえず秒数に応じて画像を表示してく
+    private float currentTime = 0f;
+
+
+
+    void Start()
+    {
+        // 初期は画像を表示しない
+        for(int i = 0;i <= 2; i++)
+        {
+            matchmakingImageArray[i].GetComponent<Image>().enabled = false;
+        }
+
+    }
+
+    
+    void Update()
+    {
+
+        // 秒数カウント開始
+        currentTime += Time.deltaTime;
+
+
+        ShowMatchmakingSuccesful();
+
+    }
+
+
+    // マッチング完了を知らせる画像を表示する関数
+    private void ShowMatchmakingSuccesful()
+    {
+
+        // 5秒経てば1つ目の画像表示
+        if (currentTime >= 5.0f)
+        {
+            matchmakingImageArray[0].GetComponent<Image>().enabled = true;
+        }
+        // 2つ目表示
+        if (currentTime >= 6.5f)
+        {
+            matchmakingImageArray[1].GetComponent<Image>().enabled = true;
+        }
+        // 3つ目表示
+        if (currentTime >= 8.0f)
+        {
+            matchmakingImageArray[2].GetComponent<Image>().enabled = true;
+        }
+
+    }
+
+
+}
