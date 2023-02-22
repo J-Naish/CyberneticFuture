676c99b27955819cacfb45d4bce23989128ad062
J-Naish <mjyrgd2332@i.softbank.jp>
2023-02-22 17:48:15 +0900

    The MiniMap Cube of Enemy only showed when player is close

---
+++ Assets/Scripts/EnemyMiniMapCube.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 敵のミニマップ表示用のCubeに関するクラス
+public class EnemyMiniMapCube : MonoBehaviour
+{
+
+    private void Start()
+    {
+        // デフォルトではCubeを非表示に
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+
+    // 範囲内にいる時のみCubeを表示させる処理
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+
+
+}
