6e0489e5101addd968a8b47059e0597909887dfa
J-Naish <mjyrgd2332@i.softbank.jp>
2023-02-26 17:33:10 +0900

    Don't generate particle when KingRobot prefab instantiated

--- Assets/Scripts/GameScene/RobotScripts/TouchGround.cs
+++ Assets/Scripts/GameScene/RobotScripts/TouchGround.cs
@@ -13,14 +13,48 @@ public class TouchGround : MonoBehaviour
     // パーティクルの発生場所
     [SerializeField] private GameObject particlePosition;
 
+    // 着地したことを検知するbool値
+    [SerializeField] private bool isTouchingGround;
+
+
+    // 時間変数
+    private float currentTime = 0f;
+
+
+    private void Update()
+    {
+        // 時間計測開始
+        currentTime += Time.deltaTime;
+
+        // 初期にパーティクルが出ないようにする
+        if(currentTime <= 1.0f)
+        {
+            isTouchingGround = false;
+        }
+
+
+        GenerateParticle();
+    }
+
+
 
     // 着地したらパーティクルをPrefab化
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            Instantiate(particle, particlePosition.transform.position, Quaternion.Euler(-90,0,0));
-            Debug.Log("ground");
+            isTouchingGround = true;
+        }
+    }
+
+
+    // パーティクルを生成する関数
+    private void GenerateParticle()
+    {
+        if (isTouchingGround)
+        {
+            Instantiate(particle, particlePosition.transform.position, Quaternion.Euler(-90, 0, 0));
+            isTouchingGround = false;
         }
     }
 
