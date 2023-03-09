f65bea4e75912cf3e64e2c532850c7245d28da85
J-Naish <mjyrgd2332@i.softbank.jp>
2023-03-09 19:02:31 +0900

    Generating MobRobot at a position where it doesn't collider anything

--- Assets/Scripts/GameScene/RobotScripts/RobotGenerator.cs
+++ Assets/Scripts/GameScene/RobotScripts/RobotGenerator.cs
@@ -12,6 +12,8 @@ public class RobotGenerator : MonoBehaviour
     [SerializeField] private GameObject rangeX2;
     [SerializeField] private GameObject rangeZ1;
     [SerializeField] private GameObject rangeZ2;
+    // Prefab生成の位置のY座標(地面)を定義
+    private const float groundYPosition = 248.5f;
 
 
     // 生成するモブロボットを取得
@@ -23,8 +25,22 @@ public class RobotGenerator : MonoBehaviour
 
 
     // MobPrefab生成のスパンと個数を定義
-    private float mobSpan = 30.0f;
+    private float mobSpan = 10.0f;
     private int mobGeneratingNumber = 10;
+
+
+    // リストを既に生成したか検知するbool値
+    private bool isAlreadyGenerated = false;
+
+    // 衝突検知オブジェクトを格納するリストを作成
+    private List<GameObject> collisionDetectorList = new List<GameObject>();
+
+    // メカボックスが他のオブジェクトとぶつからないように出現させるための空オブジェクト
+    [SerializeField] private GameObject emptyCollisionDetector;
+
+
+    // Prefabを出現させる座標
+    private float xPosition, yPosition, zPosition;
 
 
 
@@ -42,10 +58,14 @@ public class RobotGenerator : MonoBehaviour
 
 
 
+
     private void Update()
     {
-        // モブロボットを10個30秒おきに生成
-        CreateMobRobotPrefab(mobSpan,mobGeneratingNumber, mobRobotPrefab);
+        // 衝突検知オブジェクトを生成
+        CreateCollisionDetector(mobGeneratingNumber);
+
+        // モブロボットを生成
+        CreateMobRobotPrefab();
 
         // キングロボットを生成
         CreateKingRobotPrefab(kingSpan, kingRobot);
@@ -53,39 +73,95 @@ public class RobotGenerator : MonoBehaviour
 
 
 
-    // t時間おきにnr個のMobRobotを生成する関数
-    private void CreateMobRobotPrefab(float t,int n,GameObject robot)
+    private void LateUpdate()
     {
-        // 時間計測開始
+        DetectCollision();
+    }
+
+
+
+
+    // t時間おきにnr個のMobRobotを生成する関数
+    private void CreateMobRobotPrefab()
+    {
+        // 時間を加算していく
         mobCurrentTime += Time.deltaTime;
 
-
-        // span秒経過したらPrefab生成
-        if (mobCurrentTime >= t)
+        // 加算された時間がspanを超えたらPrefabを生成
+        if (mobCurrentTime > mobSpan)
         {
-            for (int i = 0; i < n; i++)
+            // 衝突検知した場所にPrefabを生成
+            for (int i = 0; i < collisionDetectorList.Count; i++)
             {
-                // robotをPrefab化
-                Instantiate(robot,GetRandomVector3(), robot.transform.rotation);
+                Instantiate(mobRobotPrefab, collisionDetectorList[i].transform.position, mobRobotPrefab.transform.rotation);
+                Destroy(collisionDetectorList[i]);
             }
 
-            // 時間をリセット
-            mobCurrentTime = 0f;
+            // リストをリセット
+            collisionDetectorList.Clear();
 
+            // リストを0から作るためのbool値を更新
+            isAlreadyGenerated = false;
+
+            // 時間リセット
+            mobCurrentTime = 0f;
         }
     }
+
+
+
+    private void CreateCollisionDetector(int n)
+    {
+        // リストが既に生成されてたら関数を実行しない
+        if (isAlreadyGenerated) return;
+
+        for (int i = 0; i < n; i++)
+        {
+            // 衝突検知用の空オブジェクトを生成
+            collisionDetectorList.Add(Instantiate(emptyCollisionDetector,
+                GetRandomVector3(), mobRobotPrefab.transform.rotation));
+        }
+
+        // bool値を変更
+        isAlreadyGenerated = true;
+    }
+
+
+
+    // 衝突検知オブジェクトが衝突してたら座標を変える関数
+    private void DetectCollision()
+    {
+        // 衝突を検知して座標を変更
+        for (int i = 0; i < collisionDetectorList.Count; i++)
+        {
+            if (collisionDetectorList[i].GetComponent<MechaBoxCollisionDetector>().isCollidered)
+            {
+                // 座標をランダムに変更
+                collisionDetectorList[i].transform.position = GetRandomVector3();
+                // 衝突検知bool値を変更
+                collisionDetectorList[i].GetComponent<MechaBoxCollisionDetector>().isCollidered = false;
+            }
+        }
+
+    }
+
 
 
 
     // ランダムな位置を取得する関数
     private Vector3 GetRandomVector3()
     {
-        Vector3 randomVector3;
-        randomVector3.x = Random.Range(rangeX1.transform.position.x, rangeX2.transform.position.x);
-        randomVector3.y = 248.5f;
-        randomVector3.z = Random.Range(rangeZ1.transform.position.z, rangeZ2.transform.position.z);
-        return randomVector3;
+        // 生成する範囲を乱数で定義
+        xPosition = Random.Range(rangeX1.transform.position.x, rangeX2.transform.position.x);
+        yPosition = groundYPosition;
+        zPosition = Random.Range(rangeZ1.transform.position.z, rangeZ2.transform.position.z);
+
+        return new Vector3(xPosition, yPosition, zPosition);
     }
+
+
+
+
 
 
 
